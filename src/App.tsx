359bc33a9a31f32f1619b5aa10ddb636114bdfeb
arybr <bagusrahadianarya@gmail.com>
2026-06-25 08:32:15 +0700

    route initiation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <div>hello world!</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/transaction-input" element={<TransactionInput />} />
+        <Route path="/history-view" element={<HistoryView />} />
+        <Route path="/configuration" element={<Configuration />} />
+        <Route path="/account" element={<Account />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
